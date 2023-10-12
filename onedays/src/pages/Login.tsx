b6580f18907941400ef5668ba1120ceaef8f37dc
Hakan ÖZER <hakanozer02@gmail.com>
2023-10-12 21:58:05 +0300

    State

--- onedays/src/pages/Login.tsx
+++ onedays/src/pages/Login.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
+
 
 function Login() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const sendLogin = (evt: React.FormEvent) => {
+    evt.preventDefault()
+    console.log(email, password)
+  }
+
   return (
-    <div>Login</div>
+    <>
+      <div className="row">
+        <div className='col-12 col-sm-12 col-md-3 col-lg-4 col-xl-4 col-xxl-4'></div>
+        <div className='col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4'>
+          <h2>User Login</h2>
+          <form onSubmit={sendLogin}>
+            <div className='mb-3'>
+              <input onChange={(evt) => setEmail(evt.target.value) } placeholder='E-Mail' type='email' className='form-control' />
+            </div>
+            <div className='mb-3'>
+              <input onChange={(evt) => setPassword(evt.target.value) } placeholder='Password' type='password' className='form-control' />
+            </div>
+            <button className='btn btn-danger'>Send</button>
+          </form>
+        </div>
+        <div className='col-12 col-sm-12 col-md-3 col-lg-4 col-xl-4 col-xxl-4'></div>
+      </div>
+    </>
   )
 }
 
